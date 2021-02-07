1bc7050643fee95efa9b95e17f861ee894544cce
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2021-02-07 18:40:27 +1300

    Updates

---
+++ precache-manifest.cbea4ed6d9f4bfc6bb5e9a53b909ba7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc97851db5c1faf1c9bcf9f172de550d",
+    "url": "/azur-nugget/index.html"
+  },
+  {
+    "revision": "203e0c7201393859bc41",
+    "url": "/azur-nugget/static/css/main.b2d20131.chunk.css"
+  },
+  {
+    "revision": "339c7e18a21de47b485c",
+    "url": "/azur-nugget/static/js/2.4d31f4bb.chunk.js"
+  },
+  {
+    "revision": "c80ac9a9381e852bfe3f140fb667e2e6",
+    "url": "/azur-nugget/static/js/2.4d31f4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203e0c7201393859bc41",
+    "url": "/azur-nugget/static/js/main.f534f101.chunk.js"
+  },
+  {
+    "revision": "8abf9a35fffe05f68aac",
+    "url": "/azur-nugget/static/js/runtime-main.700eb155.js"
+  }
+]);
